28d0c4fd10c281681a9de1ba5b084b8ba9833e32
Jakub Zasański <adnono@outlook.com>
2021-01-03 03:51:37 +0100

    Dodanie obsługi sass

---
+++ asp.net-cms/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding ProjectOpened='watch' />
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber');
+
+var paths = {
+    styles: './assets/scss/**/*.scss',
+    css: './assets/css/'
+};
+
+gulp.task('compile-scss', function () {
+ 
+    return gulp.src(paths.styles)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.styles, gulp.series('compile-scss'));
+ 
+});
