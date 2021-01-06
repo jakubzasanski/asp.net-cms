8802c23e92efa215b6d9220cf5706f390cebc635
Jakub Zasański <adnono@outlook.com>
2021-01-06 20:47:16 +0100

    brakujacy plik cs

---
+++ asp.net-cms/home.master.designer.cs
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace asp.net_cms
+{
+
+
+    public partial class home
+    {
+
+        /// <summary>
+        /// Kontrolka NewsTitle.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder NewsTitle;
+    }
+}
