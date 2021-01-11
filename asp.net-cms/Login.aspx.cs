f402671bfb61b7b9dd8b96af55a8891edbed5998
Kacper Kowalski <kkowalski2@office.cdv.pl>
2021-01-11 13:17:10 +0100

    Logowanie

---
+++ asp.net-cms/Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.net_cms
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visble = false;
+        }
+
+        protected void btnLogin_CLick(object sender, EventArgs e) 
+        {
+            
+        }
+    }
+}
